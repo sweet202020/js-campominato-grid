63aa14d264125c9bda7582c9cd86d66199944f78
sweet202020 <dolcealessio1@gmail.com>
2022-10-17 16:17:14 +0200

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,20 +7,29 @@
     -tutto quello che scrivo nella funzione sarà generato dopo il click */
     const buttonEl=document.querySelector('button');
     buttonEl.addEventListener('click', function(){
-        //il bottone genererà una griglia di gioco quadrata.
-        //sleziono l'elemento container della dom
-        //utilizzo il ciclo for
-        //aggiungo i 100 div con classe square
+        /* il bottone genererà una griglia di gioco quadrata.
+            -seleziono l'elemento container della dom
+            -utilizzo il ciclo for
+            -aggiungo i 100 div con classe square */
         const containerEl= document.querySelector('.container');
         for(i=1; i<=100; i++){
-            const squareEl = `<div class="square">${i}</div> `;
-            containerEl.insertAdjacentHTML('beforeend', squareEl);
-            console.log(containerEl);
+            //Ogni cella ha un numero progressivo, da 1 a 100 "${i}".
+            const squareEl = document.createElement('div');
+            squareEl.className='square';
+            squareEl.innerText=(i);
+            containerEl.insertAdjacentElement('beforeend', squareEl);
+            
+            
+        
+            /* Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+                -seleziono la squareEl e aggiungo addeventlistener
+                -con il this aggiungo la classe */
+
+            squareEl.addEventListener('click', function(){
+                this.classList.toggle('active')
+                console.log(this.innerText)
+            })
         }
 
-
+        
     })
-
-//Ogni cella ha un numero progressivo, da 1 a 100.
-//Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-//Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata. 
